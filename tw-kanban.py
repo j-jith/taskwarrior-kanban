3322dd0b559f5018f426a38a5fabd39538c73034
Jithin <j.jith@outlook.com>
2016-07-10 19:52:20 +0530

    Added due date to tasks due in under a week. Added warning sign for tasks due today.

--- tw-kanban.py
+++ tw-kanban.py
@@ -1,6 +1,7 @@
 import subprocess
 import json
 import jinja2
+import datetime
 
 MAX_COMPLETED = 10 # max. no. of completed tasks to display
 
@@ -16,6 +17,20 @@ def get_tasks(tags):
     tasks = json.loads(data)
 
     return tasks
+
+def check_due_date(tasks):
+    
+    for task in tasks:
+        if 'due' in task:
+            # calculate due date in days
+            due_date = datetime.datetime.strptime(task['due'], '%Y%m%dT%H%M%SZ')
+            due_in_days = (due_date - datetime.datetime.utcnow()).days
+            
+            if due_in_days > 7: # if due after a week, remove due date
+                task.pop('due', None)
+            else:
+                task['due'] = due_in_days
+
 
 def render_template(tasks_dic):
 
@@ -45,27 +60,25 @@ if __name__ == '__main__':
 
     # get tasks to do
     todo_tasks = [task for task in pending_tasks if 'start' not in task]
-    # sort tasks to do in descending order of urgency
+    # sort tasks by urgency (descending order)
     todo_tasks = sorted(todo_tasks, key=lambda task: task['urgency'], reverse=True)
+    # check due dates
+    check_due_date(todo_tasks)
 
     # get started tasks
     started_tasks = [task for task in pending_tasks if 'start' in task]
-    # sort them
+    # sort tasks by urgency (descending order)
     started_tasks = sorted(started_tasks, key=lambda task: task['urgency'], reverse=True)
+    # check due dates
+    check_due_date(started_tasks)
 
-    # add pending tasks to master dictionary (task description and project)
-    tasks_dic['todo_tasks'] = [{'description': task['description'], 'project': task['project']} \
-                               if 'project' in task else {'description': task['description']} \
-                               for task in todo_tasks]
-    tasks_dic['started_tasks'] = [{'description': task['description'], 'project': task['project']} \
-                                  if 'project' in task else {'description': task['description']} \
-                                  for task in started_tasks]
+    # add pending tasks to master dictionary 
+    tasks_dic['todo_tasks'] = todo_tasks
+    tasks_dic['started_tasks'] = started_tasks
 
     # get completed tasks and add to master dictionary (same as above)
-    completed_tasks =get_tasks(['status:completed']) 
-    tasks_dic['completed_tasks'] = [{'description': task['description'], 'project': task['project']} \
-                                    if 'project' in task else {'description': task['description']} \
-                                    for task in completed_tasks[:MAX_COMPLETED]]
+    completed_tasks = get_tasks(['status:completed']) 
+    tasks_dic['completed_tasks'] = completed_tasks[:MAX_COMPLETED]
 
     # pass master dictionary to render template and get html
     html = render_template(tasks_dic)
